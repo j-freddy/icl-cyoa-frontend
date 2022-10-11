4b485437d7251634716fcb64ab19418f0d7fd0a4
Nagpal, Prakhar <prakhar.nagpal20@imperial.ac.uk>
2022-10-11 19:57:23 +0000

    Frontend and pipeline initial setup

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>
+          Welcome to create your own adventure book!
+        </h2>
+        <a
+          href='/'
+          className="App-link"
+        >
+          Get started
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
