556f4cb28f4f7ce927d32622bc9cbcc0e59423a9
Jiang, Freddy <freddy.jiang20@imperial.ac.uk>
2023-01-10 17:13:31 +0000

    Section numbers are now in order, but sections are still shuffled. Also add redirect message for narrative-narrative connection.

--- src/utils/downloader/txtUtils.ts
+++ src/utils/downloader/txtUtils.ts
@@ -17,14 +17,20 @@ export function boxString(str: String): string {
   return box;
 }
 
-export const parseStoryToText = (story: StoryNode[]): string[] => {
+export function getShuffledId(id: number, indexCache: number[]): number {
+  // Translating between 1-index and 0-index
+  return indexCache[id - 1] + 1;
+}
+
+export const parseStoryToText
+  = (story: StoryNode[], indexCache: number[]): string[] => {
   const text: string[] = [];
 
   for (const node of story) {
     let textSection: string = "";
 
     // Section title
-    textSection += `${boxString(`Section ${node.sectionId}`)}\n\n`;
+    textSection += `${boxString(`Section ${getShuffledId(node.sectionId, indexCache)}`)}\n\n`;
 
     // Section paragraph
     textSection += `${node.paragraph}\n\n`;
@@ -36,10 +42,21 @@ export const parseStoryToText = (story: StoryNode[]): string[] => {
       textSection += `You have ${numChoices} choices:\n`;
       for (let i = 0; i < numChoices; i++) {
         textSection += `  - ${node.actions[i]}\n`;
-        textSection += `    [Go to Section ${node.childrenSectionIds[i]}]\n`;
+
+        const sectionId = node.childrenSectionIds[i];
+
+        if (sectionId !== null) {
+          textSection += `    [Go to Section ${getShuffledId(sectionId, indexCache)}]\n`;
+        }
       }
 
       textSection += "\n\n";
+    } else if (node.childrenIds.length > 0) {
+      // Narrative node connected to narrative node
+      // assert node.childrenIds.length === 1
+
+      const sectionId = story.find((storyNode) => storyNode.nodeId === node.childrenIds[0])!.sectionId;
+      textSection += `    [Go to Section ${getShuffledId(sectionId, indexCache)}]\n`;
     }
 
     text.push(textSection);
